4201004d52e0c96a157834836ea6ea9596882767
Maxime Paré <0556559@cstj.qc.ca>
2017-10-24 12:25:56 -0400

    cartes

--- Cosmos/accesBD/MySqlCarteService.cs
+++ Cosmos/accesBD/MySqlCarteService.cs
@@ -18,7 +18,7 @@ namespace Cosmos.accesBD
         /// Fonction qui retourne toutes les cartes.
         /// </summary>
         /// <returns>Liste de toutes les cartes avec leurs effets.</returns>
-        public static List<Carte> RetrieveAll()
+        private static List<Carte> RetrieveAll(string query)
         {
             List<Carte> lstResultat = new List<Carte>();
             DataSet dsResultat;
@@ -26,7 +26,7 @@ namespace Cosmos.accesBD
 
             ConnectionBD = new MySqlConnexion();
 
-            dsResultat = ConnectionBD.Query("SELECT * FROM Carte");
+            dsResultat = ConnectionBD.Query(query);
             dtResultat = dsResultat.Tables[0];
 
             foreach (DataRow dr in dtResultat.Rows)
@@ -125,11 +125,27 @@ namespace Cosmos.accesBD
         public static Carte RetrieveById(int pIdCarte)
         {
             StringBuilder query = new StringBuilder();
-            query.Append("SELECT * FROM Utilisateurs WHERE idUtilisateur = ").Append(pIdCarte.ToString());
+            query.Append("SELECT * FROM Cartes WHERE idCarte = ").Append(pIdCarte.ToString());
 
             return Retrieve(query.ToString());
 
         }
+        public static List<Carte> RetriveAllCard()
+        {
+            return RetrieveAll("SELECT * FROM Carte");
+        }
         
+        public static List<Carte> RetrieveAllUserCard(int pIdUtilisateur)
+        {
+            StringBuilder query = new StringBuilder();
+            query.Append("SELECT E.idCarte, E.quantite FROM Cartes C ")
+                 .Append("INNER JOIN Exemplaires E ON C.idCarte = E.idCarte")
+                 .Append("INNER JOIN Utilisateurs U ON u.idUtilisateur = E.idUtilisateur")
+                 .Append("WHERE E.idUtilisateur =")
+                 .Append(pIdUtilisateur.ToString());
+
+            // TODO: Prend pas en charge les quantité
+            return RetrieveAll(query.ToString());
+        }
     }
 }
