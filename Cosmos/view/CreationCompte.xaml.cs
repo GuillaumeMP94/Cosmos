bbd88ee98e6cc5dd084094a2a96c9a0d46664d6b
GuillaumeMP94 <31774008+GuillaumeMP94@users.noreply.github.com>
2017-10-27 16:27:21 -0400

    Update CreationCompte.xaml.cs

--- Cosmos/view/CreationCompte.xaml.cs
+++ Cosmos/view/CreationCompte.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Cosmos.accesBD;
+using Cosmos.metier;
 
 namespace Cosmos.view
 {
@@ -30,12 +32,97 @@ namespace Cosmos.view
 
         private void btnCreer_Click(object sender, RoutedEventArgs e)
         {
-
+            if (txbPseudo.Text.Length > 0 && passbPassword.Password.Length > 0 && passbConfirmPassword.Password.Length > 0 && txbCourriel.Text.Length > 0)
+            {
+                if (ValiderInformations())
+                {
+                    if (txbPseudo.Text.Length >=3 && passbPassword.Password.Length >= 5)
+                    {
+                        if (MySqlUtilisateurService.RetrieveByNom(txbPseudo.Text) == null && MySqlUtilisateurService.RetrieveByCourriel(txbCourriel.Text) == null)
+                        {
+                            if (passbPassword.Password == passbConfirmPassword.Password)
+                            {
+                                Utilisateur neoUtilisateur = new Utilisateur(txbPseudo.Text, txbCourriel.Text, passbPassword.Password, "qwerty");
+                                MySqlUtilisateurService.Insert(neoUtilisateur);
+                                Main.UtilisateurConnecte = MySqlUtilisateurService.RetrieveByNom(txbPseudo.Text);
+                                Main.EcranMenuPrincipal();
+                            }
+                            else
+                            {
+                                AfficherMessageErreur("motsPasseDifferents");
+                            }
+                        }
+                        else
+                        {
+                            AfficherMessageErreur("infoExistante");
+                        }
+                    }
+                    else
+                    {
+                        AfficherMessageErreur("tropCourt");
+                    }    
+                }
+                else
+                {
+                    AfficherMessageErreur("infoInvalide");
+                }
+            }
+            else
+            {
+                AfficherMessageErreur("aucuneSaisie");
+            }
         }
 
         private void btnAnnuler_Click(object sender, RoutedEventArgs e)
         {
             Main.EcranConnexion();
+        }
+        
+        private void AfficherMessageErreur(string typeErreur)
+        {
+            txbPseudo.Text = "";
+            passbPassword.Password = "";
+            passbConfirmPassword.Password = "";
+            txbCourriel.Text = "";
+
+            switch (typeErreur)
+            {
+                case "infoInvalide":
+                    txblErreur.Text = "Le nom d'utilisateur, le mot de passe ou le courriel est invalide.";
+                    break;
+                case "charInvalide":
+                    txblErreur.Text = "Le nom d'utilisateur, le mot de passe ou le courriel contient des caractères invalides.";
+                    break;
+                case "tropCourt":
+                    txblErreur.Text = "Le nom d'utilisateur ou le mot de passe est trop court.";
+                    break;
+                case "aucuneSaisie":
+                    txblErreur.Text = "Veuillez saisir toutes les informations.";
+                    break;
+                case "infoExistante":
+                    txblErreur.Text = "Le nom d'utilisateur ou le courriel est déjà utilisé.";
+                    break;
+                case "motsPasseDifferents":
+                    txblErreur.Text = "Le mot de passse de confirmation est différent du mot de passe.";
+                    break;
+            }
+
+            txblErreur.Visibility = Visibility.Visible;
+        }
+
+        private bool ValiderInformations()
+        {
+            if (Main.ValiderChampSaisi(txbPseudo.Text) == txbPseudo.Text 
+                && Main.ValiderChampSaisi(passbPassword.Password) == passbPassword.Password 
+                && Main.ValiderChampSaisi(passbConfirmPassword.Password) == passbConfirmPassword.Password
+                && Main.estCourrielValide(txbCourriel.Text))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
