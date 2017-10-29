4b32cc92ce6baedbc734f2043a15d79675d0c016
Simon Ménard <menard.simon@gmx.com>
2017-10-28 21:45:27 -0400

    Update MainWindow.xaml.cs

--- Cosmos/MainWindow.xaml.cs
+++ Cosmos/MainWindow.xaml.cs
@@ -116,6 +116,7 @@ namespace Cosmos
         {
             grdMain.Children.Remove(ContenuEcran);
             ContenuEcran = new ReglementsTutoriel(this);
+            grdMain.Children.Add(ContenuEcran);
         }
         public void EcranPartie()
         {
