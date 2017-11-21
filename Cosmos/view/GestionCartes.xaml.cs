9cfdd7fa71a194a64ed91681e248aa7a80ae805e
Guillaume MARINIER-PICHÉ <1129810@cstj.qc.ca>
2017-11-21 17:59:33 -0500

    Liste de toutes les cartes

---
+++ Cosmos/view/GestionCartes.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Cosmos.metier;
+using Cosmos.accesBD;
+using System.Linq;
+
+namespace Cosmos.view
+{
+    /// <summary>
+    /// Logique d'interaction pour GestionCartes.xaml
+    /// </summary>
+    public partial class GestionCartes : UserControl
+    {
+        private MainWindow Main;
+        private List<Carte> LstCartesCollection = MySqlCarteService.RetrieveAllCard();
+        public GestionCartes(MainWindow main)
+        {
+            InitializeComponent();
+
+            Main = main;
+
+            LstCartesCollection = TrierOrdreAlphabetique("croissant");
+
+            GenererListeCartes();
+
+        }
+
+        private void btnMenuPrincipal_Click(object sender, RoutedEventArgs e)
+        {
+            Main.EcranMenuPrincipal();
+        }
+
+        private void GenererListeCartes()
+        {
+            int compteurX = 0;
+            int compteurY = 0;
+            foreach (Carte uneCarte in LstCartesCollection)
+            {
+
+                Image imgCarte = new Image();
+                imgCarte.Source = new BitmapImage(new Uri(@"pack://application:,,,/images/cartes/" + uneCarte.Nom + ".jpg"));
+                imgCarte.Width = 160;
+                imgCarte.Height = 190;
+                imgCarte.HorizontalAlignment = HorizontalAlignment.Center;
+                imgCarte.Cursor = Cursors.Hand;
+                imgCarte.PreviewMouseLeftButtonUp += ZoomerCarte;
+                grdLesCartes.Children.Add(imgCarte);
+                Grid.SetRow(imgCarte, compteurX);
+                Grid.SetColumn(imgCarte, compteurY);
+
+                if (compteurY == 0)
+                    imgCarte.Margin = new Thickness(5);
+
+                compteurY++;
+
+                if (compteurY == 5)
+                {
+                    compteurX++;
+                    compteurY = 0;
+                }
+            }
+        }
+
+
+        private List<Carte> TrierOrdreAlphabetique(string ordre)
+        {
+            List<Carte> lstTemp = LstCartesCollection;
+            if (ordre == "croissant")
+                return lstTemp.OrderBy(carte => carte.Nom).ToList();
+            else
+                return lstTemp.OrderByDescending(carte => carte.Nom).ToList();
+        }
+
+        private void ZoomerCarte(object sender, MouseEventArgs e)
+        {
+            Image image = (Image)sender;
+
+            rectZoom.Visibility = Visibility.Visible;
+
+            imgZoomCarte.Source = image.Source;
+            imgZoomCarte.Visibility = Visibility.Visible;
+        }
+
+        private void rectZoom_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            rectZoom.Visibility = Visibility.Hidden;
+            imgZoomCarte.Visibility = Visibility.Hidden;
+        }
+    }
+}
